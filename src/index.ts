22cd7f82c63d4075e2a37783c02c935aacc21f16
Elias Koivuporras <eppu2002@gmail.com>
2023-09-09 20:04:15 +0300

    test2

--- src/index.ts
+++ src/index.ts
@@ -60,7 +60,7 @@ app.get(
   async (_req: Request, res: Response) => {
     try {
       const shopItems = await ShopItem.find();
-      console.log("JOUJOU")
+      console.log("JOUJOUo")
       res.status(200).json(shopItems);
     } catch (error) {
       console.error(error);
